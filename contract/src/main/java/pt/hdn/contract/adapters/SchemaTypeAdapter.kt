6ead67799d8749622b20f66f6f5db5d3a94dcec1
HDN Admin <hdn.company.general@gmail.com>
2021-03-19 15:26:08 +0000

    +/- SchemaTypeAdapter.kt +/- raw.xml

--- contract/src/main/java/pt/hdn/contract/adapters/SchemaTypeAdapter.kt
+++ contract/src/main/java/pt/hdn/contract/adapters/SchemaTypeAdapter.kt
@@ -1,8 +1,12 @@
 package pt.hdn.contract.adapters
 
 import com.google.gson.*
-import pt.hdn.contract.annotations.Parameter
-import pt.hdn.contract.annotations.SchemaType
+import pt.hdn.contract.annotations.Parameter.Companion.ID
+import pt.hdn.contract.annotations.SchemaType.Companion.FIX
+import pt.hdn.contract.annotations.SchemaType.Companion.RATE
+import pt.hdn.contract.annotations.SchemaType.Companion.COMMISSION
+import pt.hdn.contract.annotations.SchemaType.Companion.OBJECTIVE
+import pt.hdn.contract.annotations.SchemaType.Companion.THRESHOLD
 import pt.hdn.contract.schemas.*
 import java.lang.reflect.Type
 
@@ -11,12 +15,12 @@ class SchemaTypeAdapter : JsonDeserializer<Schema> {
         return json
             ?.let {
                 with(it.asJsonObject) {
-                    when(this[Parameter.ID].asInt) {
-                        SchemaType.FIX -> FixSchema
-                        SchemaType.RATE -> RateSchema
-                        SchemaType.COMMISSION -> CommissionSchema
-                        SchemaType.OBJECTIVE -> ObjectiveSchema
-                        SchemaType.THRESHOLD -> ThresholdSchema
+                    when(this[ID].asInt) {
+                        FIX -> FixSchema
+                        RATE -> RateSchema
+                        COMMISSION -> CommissionSchema
+                        OBJECTIVE -> ObjectiveSchema
+                        THRESHOLD -> ThresholdSchema
                         else -> null
                     }?.deserialize(this)
                 }
